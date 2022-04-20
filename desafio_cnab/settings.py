30d1cbc76e70567df159e2d402bb49257af9501d
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-20 17:55:18 -0300

    feat: adding static files dir on settings.py, adding jquery file on static folder, and importing static files on index template

--- desafio_cnab/settings.py
+++ desafio_cnab/settings.py
@@ -130,6 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'front_end', STATIC_URL)]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
