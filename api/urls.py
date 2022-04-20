ce93bf19153437c79b18c5f93d08b1d7de880545
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-20 15:58:18 -0300

    feat: registering FileUploadView route on api urls

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
+from django.urls import re_path, path
 
 from .viewsets import DocumentViewSet
+from .views import FileUploadView
+from django.views.generic import TemplateView
 
 router = DefaultRouter()
 
-router.register(r'files', DocumentViewSet)
+router.register(
+        r'files', DocumentViewSet,
+    )
 
 urlpatterns = [
-    url('', include(router.urls))
+    url('', include(router.urls)),
+    re_path(r'^upload/(?P<filename>[^/]+)$', FileUploadView.as_view(), name='file_upload_view')
 ]
