7b2549acf392f4519d55c8cb4f6eea5023e207c9
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-20 20:09:48 -0300

    feat: refactoring index template and main jquery

--- front_end/static/js/main.js
+++ front_end/static/js/main.js
@@ -3,16 +3,24 @@
         event.preventDefault();
         
         var data = new FormData(this);
+        data += "Content-Disposition: form-data; name=\"query\"; \r\n";
 
         var action = function(data) {
             console.log(data)
+            console.log(myUrl)
         }
 
+        var filename = $('input[type=file]').val().split('\\').pop();
+        var myUrl = '/api/upload/'
+
+        console.log(myUrl)
+
         $.ajax({
-            url: '/api/upload/',
-            data:data,
+            url: myUrl,
+            body: data,
             type: "POST",
             contentType: false,
+            contentDispostion: "CNAB.txt",
             processData: false,
             success: action,
             error: action,
