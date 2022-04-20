a60bfc079d0e620f94c6da13882b974eed62ee8a
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-20 15:56:28 -0300

    feat: registering templates directory on project settings file

--- desafio_cnab/settings.py
+++ desafio_cnab/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'desafio_cnab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'front_end', 'templates', 'front_end'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
