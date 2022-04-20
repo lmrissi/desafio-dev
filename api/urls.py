6e476753cccf5884e6431eb9aa3105d0b70b2b99
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-20 20:09:14 -0300

    feat: refactoring FileUploadView view and url

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import include, url
 from rest_framework.routers import DefaultRouter
-from django.urls import re_path, path
+from django.urls import path
 
 from .views import FileUploadView
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    re_path(r'^upload/(?P<filename>[^/]+)$', FileUploadView.as_view(), name='file_upload_view')
+    path('upload/', FileUploadView.as_view(), name='file_upload_view')
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 from rest_framework import status
@@ -8,15 +8,13 @@ from .models import Cnab
 from .utils import parse_cnab_file
 
 class FileUploadView(APIView):
-    parser_class = [FileUploadParser]
+    parser_classes = [MultiPartParser, FormParser]
 
     def post(self, request, format=None):
         if 'file' not in request.data:
             raise ParseError("Empty content")
-
-        file_obj = request.data['file']
         
-        pased_cnab_dict_list = parse_cnab_file(file_obj)
+        pased_cnab_dict_list = parse_cnab_file(request.data['file'])
 
         Cnab.objects.save_parsed_cnab(pased_cnab_dict_list)
 
