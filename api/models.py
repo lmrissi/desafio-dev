2d26cd4af3c8115ff5a1189d6b7c607d5961b89f
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-19 23:19:25 -0300

    feat: creating Document model and serializer

--- api/models.py
+++ api/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents', max_length=100, blank=True)
