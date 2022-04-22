135b7d0b63d94d73410b327e2e9adba95f054dd1
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-22 12:51:02 -0300

    feat: refactoring models to separate store in a new model. Implementing get_values method on Cnab model to return positive or negative values. Implementing calculate_total method on Store model to return the balance of the store

--- api/models.py
+++ api/models.py
@@ -1,29 +1,33 @@
+from tkinter import CASCADE
 from django.db import models
 from datetime import time
 
-class CnabManager(models.Manager):
-    def save_parsed_cnab(self, parsed_cnab_dict_list):
-        for dictionary in parsed_cnab_dict_list:
-            cnab = self.create(
-                type=dictionary['type'],
-                date=dictionary['date'],
-                value=dictionary['value'],
-                cpf=dictionary['cpf'],
-                card=dictionary['card'],
-                hour=dictionary['hour'],
-                store_owner=dictionary['store_owner'],
-                store_name=dictionary['store_name']
-            )
-        return cnab
+class CnabTransactions(models.Model):
+    type = models.IntegerField(unique=True)
+    description = models.CharField(max_length=30)
+    nature = models.CharField(max_length=10)
+    signal = models.CharField(max_length=1)
+
+class Store(models.Model):
+    owner = models.CharField(max_length=14)
+    name = models.CharField(max_length=19)
+
+    def calculate_total(self):
+        cnabs = Cnab.objects.filter(store=self)
+        for cnab in cnabs:
+            return sum(cnab.get_values())
 
 class Cnab(models.Model):
-    type = models.IntegerField()
+    type = models.ForeignKey(CnabTransactions, to_field="type", on_delete=models.CASCADE)
     date = models.DateField()
     value = models.DecimalField(max_digits=10, decimal_places=2)
     cpf = models.CharField(max_length=11)
     card = models.CharField(max_length=12)
     hour = models.TimeField()
-    store_owner = models.CharField(max_length=14)
-    store_name = models.CharField(max_length=19)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)
 
-    objects = CnabManager()
+    def get_values(self):
+        if self.type.nature == 'Entrada':
+            return self.value
+        else:
+            return self.value*(-1)
