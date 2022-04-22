24da0879553f4cae6a54d648ec475625e3d516e3
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-22 12:51:28 -0300

    feat: add list endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-from .views import FileUploadView
+from .views import FileUploadView, ListTransactionsView
 
 urlpatterns = [
-    path('upload/', FileUploadView.as_view(), name='file_upload_view')
+    path('upload/', FileUploadView.as_view(), name='file_upload_view'),
+    path('list/', ListTransactionsView.as_view(), name='list_transactions_view')
 ]
