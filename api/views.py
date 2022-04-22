a4ad66ea7a7a29c63af6fa2d40ab3ddcb71dbd3b
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-22 12:52:05 -0300

    feat: creating ListTransactionsView to list transactions by store

--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
+from audioop import avg
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 from rest_framework import status
 
-from .models import Cnab
-from .utils import parse_cnab_file
+from .models import Cnab, Store
+from .utils import parse_cnab_file, save_parsed_cnab, is_store_saved, list_cnabs_by_store
 
 class FileUploadView(APIView):
     parser_classes = [MultiPartParser, FormParser]
@@ -16,6 +17,18 @@ class FileUploadView(APIView):
         
         parsed_cnab_dict_list = parse_cnab_file(request.data['file'])
 
-        Cnab.objects.save_parsed_cnab(parsed_cnab_dict_list)
+        is_store_saved(parsed_cnab_dict_list)
+
+        save_parsed_cnab(parsed_cnab_dict_list)
 
         return Response(status=status.HTTP_201_CREATED)
+
+class ListTransactionsView(APIView):
+    def get(self, request):
+        stores = Store.objects.all()
+
+        cnabs = Cnab.objects.all().order_by('date')
+
+        result = list_cnabs_by_store(stores, cnabs)
+        
+        return Response(result, status=status.HTTP_200_OK)
