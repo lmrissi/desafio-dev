ebf53002e4348b4d2040d691cb8037560d7329c5
Leonardo Rissi <leonardomelorissi@gmail.com>
2022-04-22 12:55:39 -0300

    fix: fixing jquery on main.js file

--- front_end/static/js/main.js
+++ front_end/static/js/main.js
@@ -3,27 +3,13 @@
         event.preventDefault();
         
         var data = new FormData(this);
-        data += "Content-Disposition: form-data; name=\"query\"; \r\n";
-
-        var action = function(data) {
-            console.log(data)
-            console.log(myUrl)
-        }
-
-        var filename = $('input[type=file]').val().split('\\').pop();
-        var myUrl = '/api/upload/'
-
-        console.log(myUrl)
 
         $.ajax({
-            url: myUrl,
+            url: '/api/upload/',
             body: data,
             type: "POST",
             contentType: false,
-            contentDispostion: "CNAB.txt",
             processData: false,
-            success: action,
-            error: action,
         })
     })
 })
